2a5f5dbce6cdc71201b341e5e81e1facb6b40a0a
Yoga Julian Prasutiyo <yogajprasutiyo@gmail.com>
2022-01-24 10:06:11 +0700

    tugas2

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let tahun = 2022;
+let nama = "Yoga Julian Prasutiyo";
+let bumi = true;
+let buah ;
+let keranjang = null;
+let hariSepekan = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"];
+
+console.log(tahun);
+console.log(nama);
+console.log(bumi);
+console.log(buah);
+console.log(keranjang);
+console.log(hariSepekan);
